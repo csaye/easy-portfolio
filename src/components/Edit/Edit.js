53a2d026ac60f8c00ede7b80657bd85425ef30a2
Cooper Saye <coopersaye@gmail.com>
2021-01-21 14:58:00 -0800

    Implement portfolio title, subtitle, and content.

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -1,7 +1,84 @@
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+
 function Edit() {
+  const [title, setTitle] = useState('');
+  const [subtitle, setSubtitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const [loading, setLoading] = useState(true);
+
+  async function getPortfolioData() {
+    const uid = 'test-uid';
+    const doc = await firebase.firestore().collection('portfolios').doc(uid).get();
+    const data = doc.data();
+    // set parameters from data
+    setTitle(data.title);
+    setSubtitle(data.subtitle);
+    setContent(data.content);
+    // set loading false
+    setLoading(false);
+  }
+
+  // get portfolio data on start
+  useEffect(() => {
+    getPortfolioData();
+  }, []);
+
+  async function publishPortfolio(e) {
+    e.preventDefault();
+    // update portfolio
+    const uid = 'test-uid';
+    await firebase.firestore().collection('portfolios').doc(uid).update({
+      title: title,
+      subtitle: subtitle,
+      content: content
+    });
+  }
+
+  // if loading, wait
+  if (loading) {
+    return (
+      <div className="Edit">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
   return (
     <div className="Edit">
       <p>Editing portfolio</p>
+      <form onSubmit={publishPortfolio}>
+        {/* Title */}
+        <label htmlFor="titleInput">Title</label>
+        <input
+        value={title}
+        type="text"
+        id="titleInput"
+        placeholder="Title"
+        onChange={e => setTitle(e.target.value)}
+        />
+        {/* Subtitle */}
+        <label htmlFor="subtitleInput">Subtitle</label>
+        <input
+        value={subtitle}
+        type="text"
+        id="subtitleInput"
+        placeholder="Subtitle"
+        onChange={e => setSubtitle(e.target.value)}
+        />
+        {/* Content */}
+        <label htmlFor="contentInput">Content</label>
+        <input
+        value={content}
+        type="text"
+        id="contentInput"
+        placeholder="Content"
+        onChange={e => setContent(e.target.value)}
+        />
+        {/* Publish */}
+        <button type="submit">Publish</button>
+      </form>
     </div>
   );
 }
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,7 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+
 function Portfolio() {
+  const [data, setData] = useState(undefined);
+
+  async function getPortfolioData() {
+    const uid = 'test-uid';
+    const doc = await firebase.firestore().collection('portfolios').doc(uid).get();
+    setData(doc.data());
+  }
+
+  // get portfolio data on start
+  useEffect(() => {
+    getPortfolioData();
+  }, []);
+
+  // wait if no data yet
+  if (!data) {
+    return (
+      <div className="Portfolio">
+        <p>Retrieving portfolio...</p>
+      </div>
+    )
+  }
+
   return (
     <div className="Portfolio">
-      <p>Portfolio</p>
+      <h1>{data.title}</h1>
+      <h2>{data.subtitle}</h2>
+      <p>{data.content}</p>
     </div>
   );
 }

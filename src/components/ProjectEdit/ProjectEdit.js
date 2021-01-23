1bb8a25fa5035ea0d8ed819c829129d1cd3e354a
Cooper Saye <coopersaye@gmail.com>
2021-01-23 13:46:11 -0800

    Improve project editing.

--- src/components/ProjectEdit/ProjectEdit.js
+++ src/components/ProjectEdit/ProjectEdit.js
@@ -1,5 +1,6 @@
+import './ProjectEdit.css';
 import firebase from 'firebase/app';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function ProjectEdit(props) {
   // get uid and project id
@@ -8,35 +9,63 @@ function ProjectEdit(props) {
 
   const [title, setTitle] = useState(props.data.title);
   const [description, setDescription] = useState(props.data.description);
-  const [image, setImage] = useState(props.data.image);
+  const [image, setImage] = useState(undefined);
   const [url, setUrl] = useState(props.data.url);
 
-  async function updateProject(e) {
+  const [saved, setSaved] = useState(false);
+
+  async function saveProject(e) {
     e.preventDefault();
+    setSaved(false);
     // update storage with image
     if (image) {
-      const storageRef = firebase.storage().ref(uid + '/' + id);
+      const storageRef = firebase.storage().ref(uid + '/' + id + '/image');
       await storageRef.put(image);
     }
     // update project
+    const validUrl =
+    (url.startsWith('http://') || url.startsWith('https://')) ?
+    url : 'https://' + url;
     await firebase.firestore().collection('projects').doc(id).update({
       title: title,
       description: description,
-      url: url
+      url: validUrl
     });
+    setSaved(true);
   }
 
+  const [imgUrl, setImgUrl] = useState('');
+  async function getImgUrl() {
+    // if image exists
+    const listResult = await firebase.storage().ref(uid + '/' + id).listAll();
+    if (listResult.items.length > 0) {
+      // get image url
+      const iUrl = await firebase.storage()
+      .ref(uid + '/' + id + '/image').getDownloadURL();
+      setImgUrl(iUrl);
+    }
+  }
+
+  // get image url on start
+  useEffect(() => {
+    getImgUrl();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   async function deleteProject() {
-    // delete image
-    await firebase.storage().ref(uid + '/' + id).delete()
-    .catch(e => console.log('No image found to delete. This is fine.'));
+    // if image exists
+    const listResult = await firebase.storage().ref(uid + '/' + id).listAll();
+    if (listResult.items.length > 0) {
+      // delete image
+      await firebase.storage().ref(uid + '/' + id + '/image').delete();
+    }
     // delete project
     await firebase.firestore().collection('projects').doc(id).delete();
   }
 
   return (
     <div className="ProjectEdit">
-      <form onSubmit={updateProject}>
+      <form onSubmit={saveProject}>
         {/* Title */}
         <label htmlFor={`titleInput-${id}`}>Title</label>
         <input
@@ -59,7 +88,7 @@ function ProjectEdit(props) {
         <label htmlFor={`urlInput-${id}`}>URL</label>
         <input
         value={url}
-        type="url"
+        type="text"
         id={`urlInput-${id}`}
         placeholder="Project URL"
         onChange={e => setUrl(e.target.value)}
@@ -73,8 +102,14 @@ function ProjectEdit(props) {
         className="file-input"
         onChange={e => setImage(e.target.files[0])}
         />
-        {/* Publish */}
-        <button type="submit">Publish</button>
+        {
+          image ? <img src={URL.createObjectURL(image)} alt="" /> :
+          <img src={imgUrl} alt="" />
+        }
+        {/* Save */}
+        <button type="submit">Save Project</button>
+        {/* Save text */}
+        { saved && <p className="text-success">Project saved successfully</p> }
       </form>
       {/* Delete */}
       <button onClick={deleteProject}>Delete</button>
